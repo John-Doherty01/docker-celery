62ca303fa53d04af4c1e22ac8f0d22dcac446fb5
John Doherty <jjdoherty98@gmail.com>
2022-03-27 13:10:21 +0100

    Kubectl apply updated

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,6 @@ ENV_TYPE=prod
 BUILD_TAG=hitman-$ENV_TYPE-$BUILD_NUMBER
 REGISTRY=ghcr.io/john-doherty01
 
-sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
-mkdir ~/.kube/
-touch ~/.kube/config
-echo $KUBECTL_CONFIG > ~/.kube/config
-
-sed -i "s/VERSION/${BUILD_TAG}/g" ./docker/rest-api/rest-api.yaml
-sed -i "s/VERSION/${BUILD_TAG}/g" ./docker/celery-worker/celery-worker.yaml
-sed -i "s/VERSION/${BUILD_TAG}/g" ./docker/celery-flower/celery-flower.yaml
-
 echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin
 
 docker build -f ./docker/rest-api/Dockerfile . -t hitman/rest-api
@@ -31,7 +18,3 @@ docker push $REGISTRY/hitman-celery-worker:$BUILD_TAG
 docker build -f ./docker/celery-flower/Dockerfile . -t hitman/celery-flower
 docker tag hitman/celery-flower $REGISTRY/hitman-celery-flower:$BUILD_TAG
 docker push $REGISTRY/hitman-celery-flower:$BUILD_TAG
-
-kubectl apply -f ./docker/rest-api/rest-api.yaml
-kubectl apply -f ./docker/celery-worker/celery-worker.yaml
-kubectl apply -f ./docker/celery-flower/celery-flower.yaml
