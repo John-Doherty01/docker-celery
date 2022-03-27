c579a4ca89539cfefee1e39f91db75696245baaf
John Doherty <jjdoherty98@gmail.com>
2022-03-27 12:16:38 +0100

    Create file

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,7 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
+touch ~/.kube/config
 echo $KUBECTL_CONFIG | tee ~/.kube/config
 
 sed -i "s/VERSION/${BUILD_TAG}/g" ./docker/rest-api/rest-api.yaml
