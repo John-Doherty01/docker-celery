a2863ed0bf189c09f90ae94b1e442d269a50d0a4
John Doherty <jjdoherty98@gmail.com>
2022-03-27 12:18:32 +0100

    Mkdir added

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,7 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
+mkdir ~/.kube/
 touch ~/.kube/config
 echo $KUBECTL_CONFIG | tee ~/.kube/config
 
