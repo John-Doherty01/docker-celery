c9b66170f06fd4dd2fefef7cf6e45f767f34dedf
John Doherty <jjdoherty98@gmail.com>
2022-04-03 17:52:43 +0100

    Docker build command

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,10 @@ REGISTRY=ghcr.io/john-doherty01
 
 echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin
 
+docker build -f ./docker/webapp/Dockerfile . -t hitman/webapp --build-arg REACT_ENV_PROD="$REACT_ENV_PROD"
+docker tag hitman/webapp $REGISTRY/hitman-webapp:$BUILD_TAG
+docker push $REGISTRY/hitman-webapp:$BUILD_TAG
+
 docker build -f ./docker/rest-api/Dockerfile . -t hitman/rest-api
 docker tag hitman/rest-api $REGISTRY/hitman-rest-api:$BUILD_TAG
 docker push $REGISTRY/hitman-rest-api:$BUILD_TAG
@@ -18,7 +22,3 @@ docker push $REGISTRY/hitman-celery-worker:$BUILD_TAG
 docker build -f ./docker/celery-flower/Dockerfile . -t hitman/celery-flower
 docker tag hitman/celery-flower $REGISTRY/hitman-celery-flower:$BUILD_TAG
 docker push $REGISTRY/hitman-celery-flower:$BUILD_TAG
-
-docker build -f ./docker/webapp/Dockerfile . -t hitman/webapp --build-arg REACT_ENV_PROD=$REACT_ENV_PROD
-docker tag hitman/webapp $REGISTRY/hitman-webapp:$BUILD_TAG
-docker push $REGISTRY/hitman-webapp:$BUILD_TAG
