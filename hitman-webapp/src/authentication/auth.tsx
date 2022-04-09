7a1bdf4ff435291289e3afaa0a1185f0ee011695
John Doherty <jjdoherty98@gmail.com>
2022-04-09 09:35:44 +0100

    Docker build updated

--- hitman-webapp/src/authentication/auth.tsx
+++ hitman-webapp/src/authentication/auth.tsx
@@ -1,5 +1,10 @@
-export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-export const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
-export const HOST_URL = process.env.REACT_APP_API_HOST_URL;
-export const WEBSOCKET_URL = process.env.REACT_APP_WEBSOCKET_URL;
+export const CLIENT_ID =
+  process.env.REACT_APP_CLIENT_ID || "FSb5TMUaS4GSdaFjj1M8kfwec2vxe7EH3eO8pdcx";
+export const CLIENT_SECRET =
+  process.env.REACT_APP_CLIENT_SECRET ||
+  "0KobqEc4etm10YU3IeEdJ9k0Pv6GTuUl8UO15WnrlkkIgJ9MgVZqzCApFTWSaURudxlDj3gIUJcwkPXcORV1i1kcGrEXDXF2rxNMWtm4Hr6iuNDncp3c5g7P5BMY8mo7";
+export const HOST_URL =
+  process.env.REACT_APP_API_HOST_URL || "http://localhost:8000/";
+export const WEBSOCKET_URL =
+  process.env.REACT_APP_WEBSOCKET_URL || "ws://localhost:8000/";
 export const TOKEN_LOCALSTORAGE_KEY = "TOKEN";
