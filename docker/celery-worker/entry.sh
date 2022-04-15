2de26078ee72b947d06e0a739db812b794b8cc41
John Doherty <jjdoherty98@gmail.com>
2022-04-15 18:08:56 +0100

    Celery config updated

--- docker/celery-worker/entry.sh
+++ docker/celery-worker/entry.sh
@@ -8,4 +8,4 @@ python manage.py collectstatic --clear --noinput
 
 echo "Starting celery worker"
 /etc/init.d/celerybeat start
-exec celery -A findmyhitman worker -Q celery --loglevel=info
+exec celery -A findmyhitman worker -Q celery --loglevel=info --concurrency=1 --max-memory-per-child=25000
